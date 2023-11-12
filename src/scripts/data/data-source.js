9b1112b36bd6889599c698d555e9dd5a4702cdae
Leonardo Denavito <jounandavito@gmail.com>
2023-11-13 03:50:49 +0700

    Updated the AJAX functions Updated HTML element for empty search view Added Loader Added style for loader

--- src/scripts/data/data-source.js
+++ src/scripts/data/data-source.js
@@ -1,14 +1,20 @@
 import API_ENDPOPINTS from "../global/api-endpoints";
 import OPTIONS from "../global/fetch-options";
+import Loader from "../utils/loader-performer";
 
 class DataSource {
     static async allDocuments() {
+        Loader.performLoader();
         try {
             const response = await fetch(API_ENDPOPINTS.SHOW_ALL_DOCS, OPTIONS);
             const responseJson = await response.json();
 
-            return responseJson.documents;
+            Loader.finishLoader();
+
+            return await responseJson.documents;
         } catch (error) {
+            Loader.finishLoader();
+            
             return 'gagal'
         }
     }
