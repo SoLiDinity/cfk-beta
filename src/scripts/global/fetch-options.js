73f278dd43f8ecf6d4d713fdf1513a5223781e70
Leonardo Denavito <jounandavito@gmail.com>
2023-11-13 03:58:36 +0700

    Updated fetch options

--- src/scripts/global/fetch-options.js
+++ src/scripts/global/fetch-options.js
@@ -2,9 +2,7 @@ const OPTIONS = {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
-        "Access-Control-Request-Headers": "*",
-        "api-key": "l8XUcaftkQlJp4m4AWgLl7kAHbe26UbEO34HQLTjfd5IpEAGoGofsvPFxg4BT6D7",
-        "Accept": "application/json"
+        "api-key": "l8XUcaftkQlJp4m4AWgLl7kAHbe26UbEO34HQLTjfd5IpEAGoGofsvPFxg4BT6D7"
     },
     body: JSON.stringify({
         "dataSource": "Cluster0",
